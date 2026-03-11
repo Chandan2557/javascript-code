b32c5e9c9eb109e8f01325430b8f08e8e6a7cc56
Chandan meena <chandanmeena7354@gmail.com>
2026-03-11 10:10:16 +0530

    add file

---
+++ Class-02/script.js
@@ -0,0 +1,72 @@
+let x=4;
+let y=5;
+console.log(x+y);
+
+let m=6;
+let n=8;
+console.log(m-n);
+
+let r=9;
+let p=3;
+console.log(r*p);
+
+let a=2;
+let c=5;
+console.log(a/c);
+
+let w=10;
+let z=5;
+console.log(w%z);
+
+var v = 10;
+console.log(typeof(x));
+
+const name = "Chandan";
+console.log(typeof(name));
+
+const u = undefined ;
+console.log(typeof(u));
+
+let d =10;
+d+=5;
+console.log(d);
+
+let i = 4;
+let j = 7;
+console.log(i > j);
+
+var A = 12;
+var B = 18;
+console.log(12 > 18 && 18 > 12);
+
+var A = 12;
+var B = 18;
+console.log(12 > 18 || 18 > 12);
+
+var A = 12;
+var B = 18;
+console.log(12 > 18 != 18 > 12);
+
+
+var N = 15;
+console.log(N++);
+
+var E = 15;
+console.log(++E);
+
+var K = 30;
+console.log(--K)
+
+var L = 30;
+console.log(L--)
+
+
+let Y =5;
+
+console.log (++Y + Y-- + --Y - Y++ + Y-- - --Y + Y--);
+console.log(Y);
+
+
+let H = 10;
+H += 5;
+console.log(H)
