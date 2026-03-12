035768032f33c4ff01fb1a84ff11c9dfc1e582b4
Chandan meena <chandanmeena7354@gmail.com>
2026-03-12 09:56:53 +0530

    changes

---
+++ Class-04/script.js
@@ -0,0 +1,25 @@
+//1. 1 to 20
+/*
+let num = prompt("Enter Number");
+for( let i=1; i<=num; i++){
+    console.log(i);
+}
+
+//2. reverse 20 to 1
+let num = prompt("Enter Number");
+for( let i=20; i>=num; i++){
+    console.log(i);
+}
+*/
+
+//3. Find sum 1 to 100
+let num = prompt("Enter Number");
+for (let i = 1; i <= num; i++) {
+    i+= i;
+}
+console.log(sum)
+
+
+
+
+
