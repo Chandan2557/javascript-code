e846b52d21ab487c22be4b7bef96826e4e274fe4
Chandan meena <chandanmeena7354@gmail.com>
2026-03-15 10:31:35 +0530

    Add file

---
+++ class-07/script.js
@@ -0,0 +1,38 @@
+//<------- Array in js --------->
+// Basic Array
+/*
+let x = [1, 2, 3, 4, 5];
+for (let i = 0; i < 5; i++) {
+    console.log(x[i]);
+}
+
+// Nested Array
+let z = [1, 2, [6, 7], 3, 4, 5, [9, 10]];
+
+for (let i = 0; i < z.length; i++) {
+    console.log(z[i]);
+}
+console.log(z[2][1]);
+console.log(z[2]);
+console.log(z[6]);
+console.log(z[1]);
+
+
+
+let x = [1,[2,[3,[4,[5,[6,[7,[8,[9,[10,[100, 300],[500, 1000]]]]]]]]]]];
+
+console.log(x[1][1][1][1][1][1][1][1][1][2][0]);
+
+
+let arr = [1,2,[3, 4],5];
+console.log(arr[3]);
+
+
+let arr = [1,[2,[3, [4, 5],[6,7],[8, [9, [10], 11]]]]];
+ console.log(arr[1][1][3][1][2]);
+ */
+
+ let arr = [1,[2,[3,[4,[5,[12,[13,[14,[15,16]]]]],[6,7],[8,[9,[10],11]]]]]];
+ console.log(arr[1][1][1][3][1][1][0
+    
+ ])
