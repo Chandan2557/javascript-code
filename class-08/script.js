7a86903ad5c129c07694c753be7cc2cd70bcdd94
Chandan meena <chandanmeena7354@gmail.com>
2026-03-16 16:03:39 +0530

    Add file

---
+++ class-08/script.js
@@ -0,0 +1,172 @@
+//Q1
+/*
+console.log("hey");
+{
+  let a = 1;
+  let b = 2;
+  console.log(a);
+  console.log(b);
+}
+console.log(a);
+console.log(b);
+
+//Q2
+console.log("hey");
+{
+  var a = 1;
+  var b = 2;
+  console.log(a);
+  console.log(b);
+}
+console.log(a);
+console.log(b);
+
+//Q3
+console.log(a);
+ var a=()=>{
+     console.log("hello");
+     }
+
+//Q4
+console.log(a);
+function a(){
+    console.log("hello");
+}
+
+
+//Q4
+var x = 20;
+function printValue() {
+    console.log(x);
+    var x = 10;
+}
+printValue();
+
+
+//Q5
+var x = 20;
+function printValue1() {
+    console.log(x);
+     x = 10;
+    console.log(x);
+}
+console.log(x);
+printValue1();
+
+//Q5
+console.log("start ");
+setTimeout(()=>{
+    console.log("setTime");
+},1);
+console.log("end");
+
+
+//Q6
+let y;
+console.log(y);
+console.log(typeof(y));
+
+//Q7
+function abc() {
+     a = 10;
+     b = 20;
+    console.log(a);
+    console.log(b);
+}
+abc();
+console.log(a);
+console.log(b);
+
+
+//Q8
+console.log([1,2,3] === [1,2,3]);
+console.log({ name: "a" } == { name: "a" });
+
+
+//Q9
+console.log(name);
+var name = "John";
+
+
+//Q10
+setTimeout(() => {
+  console.log("I am setTime out ");
+});
+Promise.resolve().then(() => {
+  console.log("I am Promise  ");
+});
+console.log("I am end here ");
+
+
+//Q11
+let a=10;
+console.log(++a)
+console.log(a)
+let a1=10;
+console.log(a1++)
+console.log(a1)
+
+
+//Q12
+console.log(1<2<3)
+console.log(3>2>0)
+console.log(3>2>1)
+
+
+//Q13
+const z=null; // null ka type object hota h. it's object bug.
+console.log(typeof z)
+const a=undefined;
+console.log(typeof a)
+
+
+//Q14
+function printVal(){
+  var x=10;
+  console.log(x);//10
+}
+printVal();
+console.log(x);
+
+
+//Q15
+function demo(){
+  console.log("hey");//hey print
+}
+const ans=demo();
+
+console.log(ans);
+function demo1(){
+  return "hey";
+}
+const ans1=demo1();
+console.log(ans1);
+
+
+//Q16
+console.log(a);
+console.log(b);
+var a=b=5;
+
+######################## MOST IMP QUESTION ####################################################
+
+
+//Q16
+const user1 = {
+  userName: "syam",
+  age: 20,
+  address: {
+    city: "katni",
+    state: "mp",
+  },
+};
+const user2 = user1;
+user2.userName = "Dr. K. Anji Reddy";
+user2.address.city = "indore";
+console.log("user1", user1); 
+console.log("user2", user2);
+
+*/
+//Q17
+let result = PrintValue();
+console.log(result);
